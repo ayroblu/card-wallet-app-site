6104ab86238b4cc3c60e9312cb06ba8309bc7032
Ben Lu <ayroblu@gmail.com>
2025-08-02 22:52:12 +0100

    font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,15 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<meta name="color-scheme" content="dark light" />
 </svelte:head>
 
 {@render children?.()}
+
+<style>
+	:global html {
+		font-family:
+			system-ui, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji',
+			'Segoe UI Emoji', 'Segoe UI Symbol';
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,13 @@
-<head>
-	<meta name="color-scheme" content="dark light">
-</head>
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Card Wallet App</h1>
+<p>Easily keep track of your loyalty cards</p>
+
+<a
+	href="https://apps.apple.com/us/app/x/id333903271?itscg=30200&itsct=apps_box_badge&mttnsubad=333903271"
+	style="display: inline-block;"
+>
+	<img
+		src="https://toolbox.marketingtools.apple.com/api/v2/badges/download-on-the-app-store/black/en-us?releaseDate=1255046400"
+		alt="Download on the App Store"
+		style="width: 245px; height: 82px; vertical-align: middle; object-fit: contain;"
+	/>
+</a>
