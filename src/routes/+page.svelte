56a88201d9ac37a6586471abc51b29e70d397358
Ben Lu <ayroblu@gmail.com>
2025-08-02 21:50:18 +0100

    build

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
+<head>
+	<meta name="color-scheme" content="dark light">
+</head>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
